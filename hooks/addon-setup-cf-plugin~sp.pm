e0fe44c84f6cb4e229d3aedd319b2f16b593096d
Wayne E. Seguin <wayneeseguin@gmail.com>
2025-06-19 12:53:54 -0400

    modeline to bottom, post deploy done(1)

--- hooks/addon-setup-cf-plugin~sp.pm
+++ hooks/addon-setup-cf-plugin~sp.pm
@@ -1,4 +1,3 @@
-# vim: set ts=2 sw=2 sts=2 noet fdm=marker foldlevel=1:
 package Genesis::Hook::Addon::Scheduler::SetupCFPlugin;
 
 use v5.20;
@@ -101,3 +100,4 @@ sub perform {
 }
 
 1;
+# vim: set ts=2 sw=2 sts=2 noet fdm=marker foldlevel=1:
